311af7484eba6fa3b408882cdf2b75b1a0322191
Leigh McCulloch <leigh@mcchouse.com>
2010-10-15 22:07:43 +1100

    Moved copyright to it's own file.

--- index.php
+++ index.php
@@ -49,11 +49,7 @@ $stories = get_index(INDEX_FILE_DEFAULT);
 </div>
 
 <div id="footer">
-  Thanks to the <a href="http://www.tyndale.com">Tyndale House Publishers</a> for permitting use of the index they provide with their New Living Translations Bibles.
-  
-  This website was authored by <a href="http://leighmcculloch.com">Leigh McCulloch</a> with the goal to make this useful index accessible to everyone.
-  
-  Scripture taken from The Holy Bible, English Standard Version. Copyright &copy;2001 by <a href="http://www.crosswaybibles.org">Crossway Bibles</a>, a publishing ministry of Good News Publishers. Used by permission. All rights reserved. Text provided by the <a href="http://www.gnpcb.org/esv/share/services/">Crossway Bibles Web Service</a>.
+  <?php include 'copyright.php'; ?>
 </div>
 
 <script type="text/javascript">
