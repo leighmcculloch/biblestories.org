e708c3c5ccda3f8a8264a19fb0e19ac441066fb0
Leigh McCulloch <leigh@mcchouse.com>
2010-10-16 08:40:51 +1100

    Set the viewport width for the iPhone.

--- index.php
+++ index.php
@@ -8,6 +8,7 @@ $stories = get_index(INDEX_FILE_DEFAULT);
 <HTML>
 <HEAD>
   <TITLE><?php echo TITLE_DEFAULT; ?></TITLE>
+  <meta name="viewport" content="width=device-width" />
   <link href="style.css" rel="stylesheet" type="text/css">
   <link href="http://ajax.googleapis.com/ajax/libs/jqueryui/1.8/themes/base/jquery-ui.css" rel="stylesheet" type="text/css"/>
   <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.4/jquery.min.js"></script>
