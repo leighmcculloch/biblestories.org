fa6fcca7e7546ef75bec85d9a9abb078aeaf4a4a
Leigh McCulloch <leigh@mcchouse.com>
2010-10-16 19:00:42 +1100

    Added link to github in footer.

--- footer.php
+++ footer.php
@@ -3,3 +3,5 @@ Thanks to the Tyndale House Publishing Inc. for permission to freely republish t
 This website was created by <a href="http://leighmcculloch.com">Leigh McCulloch</a> with the goal to make accessable a searchable index of some of the great stories of the Bible.
   
 Scripture taken from The Holy Bible, English Standard Version and Copyright &copy;2001 by <a href="http://www.crosswaybibles.org">Crossway Bibles</a>, a publishing ministry of Good News Publishers. Used by permission. All rights reserved. Text provided by the <a href="http://www.gnpcb.org/esv/share/services/">Crossway Bibles Web Service</a>.
+
+This whole site's source code is available at <a href="http://github.com/leighmcculloch/greatstoriesofthebible.org">github</a>.
