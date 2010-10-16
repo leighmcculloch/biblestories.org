88b5710a13c51082a945711dc7448ddc679e58b7
Leigh McCulloch <leigh@mcchouse.com>
2010-10-16 18:37:17 +1100

    Fixed up the copyright.

--- copyright.php
+++ copyright.php
@@ -1,5 +1,5 @@
-Thanks to the <a href="http://www.tyndale.com">Tyndale House Publishers</a> for permitting use of the index they provide with their New Living Translations Bibles.
+Thanks to the Tyndale House Publishing Inc. for permission to freely republish the "Great Stories of the Bible" index, which is found in many publishings of the New Living Translation Bibles. 
+
+This website was created by <a href="http://leighmcculloch.com">Leigh McCulloch</a> with the goal to make accessable a searchable index of some of the great stories of the Bible.
   
-This website was authored by <a href="http://leighmcculloch.com">Leigh McCulloch</a> with the goal to make this useful index accessible to everyone.
-  
-Scripture taken from The Holy Bible, English Standard Version. Copyright &copy;2001 by <a href="http://www.crosswaybibles.org">Crossway Bibles</a>, a publishing ministry of Good News Publishers. Used by permission. All rights reserved. Text provided by the <a href="http://www.gnpcb.org/esv/share/services/">Crossway Bibles Web Service</a>.
+Scripture taken from The Holy Bible, English Standard Version and Copyright &copy;2001 by <a href="http://www.crosswaybibles.org">Crossway Bibles</a>, a publishing ministry of Good News Publishers. Used by permission. All rights reserved. Text provided by the <a href="http://www.gnpcb.org/esv/share/services/">Crossway Bibles Web Service</a>.
