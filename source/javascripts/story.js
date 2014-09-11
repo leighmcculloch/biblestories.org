6222805fdff662800f368a69f25ba1aae3620d06
Leigh McCulloch <leigh@mcchouse.com>
2014-09-11 08:41:37 -0700

    cools

--- source/javascripts/story.js
+++ source/javascripts/story.js
@@ -15,3 +15,19 @@ function toggleSideMenu() {
     $('.wrapper').addClass('show-side-menu');
   }
 }
+
+var addthis_config = {
+  data_track_addressbar: false,
+  data_track_clickback: false
+};
+var addthis_share = {
+  url: window.location.href
+};
+$(window).ready(function() {
+  $.ajax({
+    url: '//s7.addthis.com/js/300/addthis_widget.js#pubid=ra-538af91b78db2c14',
+    cache: true,
+    async: true,
+    dataType: 'script'
+  });
+});
