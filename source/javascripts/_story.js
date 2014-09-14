d4749f7f0472d32eb01b84adee4516801e8e4152
Leigh McCulloch <leigh@mcchouse.com>
2014-09-14 00:51:23 -0700

    interesting

--- source/javascripts/_story.js
+++ source/javascripts/_story.js
@@ -1,19 +1,17 @@
 /* side menu */
 $(function() {
-    $('.toggle-side-menu')
-      .click(function() {
-        toggleSideMenu();
-      })
-      /*.bind('touchstart', function() {
-        toggleSideMenu();
-      })*/;
+    $('.toggle-side-menu').click(function() {
+      toggleSideMenu();
+    });
 });
 
 function toggleSideMenu() {
-  if ($('.wrapper').hasClass('show-side-menu')) {
-    $('.wrapper').removeClass('show-side-menu');
+  if ($('.side-menu').hasClass('show-side-menu')) {
+    $('.side-menu').removeClass('show-side-menu');
   } else {
-    $('.wrapper').addClass('show-side-menu');
+    $('.side-menu')
+      .addClass('show-side-menu')
+      .scrollTop(0);
   }
 }
 
@@ -47,3 +45,31 @@ $(function() {
     return false;
   });
 });
+
+/* audio and audio analytics */
+var audio = $('.audio-player audio').get(0);
+if (typeof audio != 'undefined') {
+  if (!!(audio.canPlayType && audio.canPlayType('audio/mpeg;').replace(/no/, ''))) {
+    $(audio).gaPlayLength({
+      audioTime: true,
+      userTiming: true
+    });
+    $('.controls .audio').click(function(ev){
+      ev.preventDefault();
+      $('.audio-player').show();
+      if (typeof audio != 'undefined') {
+        audio.addEventListener('canplay', function(e) {
+          audio.play();
+        }, false);
+        audio.load();
+      }
+    });
+  } else {
+    $('.controls .audio').click(function(ev){
+        ga('send', 'event', 'Listen', 'direct');
+    });
+  }
+}
+
+/* scroll depth analytics */
+$.scrollDepth();
--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -1,6 +1,8 @@
 /*
- *= require _jquery-1.11.1.min
- *= require _fastclick.js
+ *= require vendor/_jquery-1.11.1.min
+ *= require vendor/_fastclick.js
+ *= require vendor/_jquery.ga-playlength
+ *= require vendor/_jquery.ga-scrolldepth
  *= require _iscompact.js
  *= require _shared.js
  *= require _story.js
