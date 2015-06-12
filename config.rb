037e1b37211c67990d5033118468e24f4c2f90f4
Leigh McCulloch <leigh@mcchouse.com>
2015-06-11 22:39:54 -0700

    Delete from S3 when deploying in DEV

--- config.rb
+++ config.rb
@@ -182,7 +182,7 @@ activate :imageoptim unless DEV
 activate :s3_sync do |s3_sync|
   s3_sync.bucket                     = DEPLOYMENT.host
   s3_sync.region                     = "us-east-1"
-  s3_sync.delete                     = false
+  s3_sync.delete                     = DEV
   s3_sync.after_build                = true
   s3_sync.prefer_gzip                = true
   s3_sync.path_style                 = true
