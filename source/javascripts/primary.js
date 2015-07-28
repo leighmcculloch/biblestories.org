f553a4fbf7f9bf063eec79de02254e276e5ae791
Leigh McCulloch <leigh@mcchouse.com>
2015-07-28 08:03:08 -0700

    Updated to using the latest bower, and non-jQuery version of `gaplaylength`.

--- source/javascripts/primary.js
+++ source/javascripts/primary.js
@@ -2,10 +2,10 @@
  *= require vendor/_modernizr.custom
  *= require vendor/_jquery-1.11.1.min
  *= require vendor/_fastclick
- *= require vendor/_jquery.ga-playlength
  *= require vendor/_jquery.ga-scrolldepth
  *= require vendor/_jquery.medium-highlight-and-popup
  *= require vendor/_jquery.autowidth
+ *= require gaplaylength/dist/gaplaylength.min
  *= require _iscompact
  *= require _shared
  *= require _story
