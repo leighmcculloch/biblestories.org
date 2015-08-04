b4fd429c6ed9fd118bb7ba15314e87e3714356bd
Leigh McCulloch <leigh@mcchouse.com>
2015-08-03 22:05:05 -0700

    Refactored the scripts, and moved to scrolldepth without jQuery.

--- source/javascripts/01.js
+++ source/javascripts/01.js
@@ -2,7 +2,6 @@
  *= require vendor/_modernizr.custom
  *= require vendor/_jquery-1.11.1.min
  *= require vendor/_fastclick
- *= require vendor/_jquery.ga-scrolldepth
  *= require vendor/_jquery.medium-highlight-and-popup
  *= require vendor/_jquery.autowidth
  *= require 01/_iscompact
--- source/javascripts/02.js
+++ source/javascripts/02.js
@@ -28,3 +28,50 @@
 
   window.addEventListener("load", load, false);
 }());
+
+/*
+ *= require vendor/_scrolldepth
+ */
+
+(function() {
+  if (!window.addEventListener) {
+    return;
+  }
+
+  function load(event){
+    window.removeEventListener('load', load, false);
+
+    if (document.body.className.indexOf('story') === -1) {
+      return;
+    }
+
+    scrolldepth.init();
+  }
+
+  window.addEventListener('load', load, false);
+}());
+
+/*
+ *= require gaplaylength/dist/gaplaylength.min
+ */
+
+(function() {
+  if (!window.addEventListener) {
+    return;
+  }
+
+  function load(event){
+    window.removeEventListener('load', load, false);
+
+    if (document.body.className.indexOf('story') === -1) {
+      return;
+    }
+
+    var audio = document.querySelector('.audio-player audio');
+    if (audio) {
+      gaplaylength.init(audio);
+    }
+  }
+
+  window.addEventListener('load', load, false);
+}());
